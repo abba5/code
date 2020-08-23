400af1ebf71cb896f4af60253cca61f3a8d5c761
abba5 <abbastrick@gmail.com>
2020-08-24 00:04:28 +0530

    1. change name of files. 2. change template.

--- CP/lis_using_set.cpp
+++ CP/lis_using_set.cpp
@@ -2,16 +2,18 @@
 #include <set>
 #include <vector>
 
+
 int LIS(vector <int> &v){
 
 	// beautiful!!! isn't it ?
 	// https://codeforces.com/blog/entry/13225
 
 	const int n = v.size();
-	for(int i = 0; i < n; ++i){
-	    s.insert(a[i]);
+	set <int> s;
+    for(int i = 0; i < n; ++i){
+	    s.insert(v[i]);
 	    
-	    it = s.upper_bound(a[i]);
+	    auto it = s.upper_bound(v[i]);
 
 	    if(it != s.end())
 	        s.erase(it);
---
+++ CP/template.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <climits>
+#include <ctime>
+#include <cmath>
+#include <cstdlib>
+#include <iomanip>
+#include <cstring>
+
+// ---------- PBDS -----------------
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp> 
+#include <ext/pb_ds/detail/standard_policies.hpp>
+
+// ----------------------------------
+
+using namespace std;
+using namespace __gnu_pbds; 
+
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define ALL(a) a.begin(), a.end()
+#define MP(a, b) make_pair(a, b)
+#define PB(a) push_back(a)
+#define EB(a) emplace_back(a) 
+#define boost ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0),cout << fixed << setprecision(10);
+// #define ll long long int
+#define int long long int
+
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+	*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+#define deb(x) debug() << imie(x)
+
+
+void solve(){
+	
+}
+
+int32_t main(){
+
+	boost;
+
+	int T;
+	
+	cin >> T;
+	// T = 1;
+	
+	for(int i = 1; i <= T; ++i){
+		solve();
+		cout << "\n";
+	}
+
+	return 0;
+}
+
